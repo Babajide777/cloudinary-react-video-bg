1d4cf182f19963d6719171cc599b7c820b6cc23c
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2023-06-24 17:38:52 -0500

    added textoverlay

--- src/components/OtherImage.jsx
+++ src/components/OtherImage.jsx
@@ -3,9 +3,8 @@ import { AdvancedVideo } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen";
 import { fill } from "@cloudinary/url-gen/actions/resize";
 import { source } from "@cloudinary/url-gen/actions/overlay";
-import { image } from "@cloudinary/url-gen/qualifiers/source";
-import { Transformation } from "@cloudinary/url-gen";
-import { scale } from "@cloudinary/url-gen/actions/resize";
+import { text } from "@cloudinary/url-gen/qualifiers/source";
+import { TextStyle } from "@cloudinary/url-gen/qualifiers/textStyle";
 const REACT_APP_CLOUNDINARY_CLOUND_NAME =
   process.env.REACT_APP_CLOUNDINARY_CLOUND_NAME;
 
@@ -20,14 +19,9 @@ const OtherImage = () => {
   //Link for the video on Cloudinary
   const myVideo = cld.video("samples/elephants");
 
-  myVideo.resize(fill().width(500).height(300)).overlay(
-    source(
-      //Link for th image on Cloudinary
-      image("samples/ecommerce/analog-classic").transformation(
-        new Transformation().resize(scale().width(150))
-      )
-    )
-  );
+  myVideo
+    .resize(fill().width(500).height(300))
+    .overlay(source(text("OYAFEMI", new TextStyle("Arial", 80))));
 
   return (
     <div className="video">
